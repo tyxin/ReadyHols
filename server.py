49bff7b32f887c6d53af068d8a04161a08cd15aa
tyxin <teohyuxin2015@gmail.com>
2023-02-20 15:50:06 +0800

    Flask Update - Navigation

--- server.py
+++ server.py
@@ -39,5 +39,37 @@ def sign_up():
 def tutorial():
     return render_template('/n-login/tutorial.html')
 
+@app.route('/logged/home')
+def logged_home():
+    return render_template('/login/vacations/vacations.html')
+
+@app.route('/logged/vacations')
+def logged_vacations():
+    return render_template('/login/vacations/vacations.html')
+
+@app.route('/logged/user')
+def logged_user():
+    return render_template('/login/user/user.html')
+
+@app.route('/logged/settings')
+def logged_settings():
+    return render_template('/login/settings/settings.html')
+
+@app.route('/logged/vacations/summary')
+def logged_vacations_summary():
+    return render_template('/login/vacations/summary/summary.html')
+
+@app.route('/logged/vacations/itinerary')
+def logged_vacations_itinerary():
+    return render_template('/login/vacations/itinerary/itinerary.html')
+
+@app.route('/logged/vacations/planning')
+def logged_vacations_planning():
+    return render_template('/login/vacations/planning/planning.html')
+
+@app.route('/logged/vacations/sharing')
+def logged_vacations_sharing():
+    return render_template('/login/vacations/sharing/sharing.html')
+
 if __name__ == '__main__':
     app.run()
